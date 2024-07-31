827136ccdd56fac9a9341e810345025bcf9abd05
YehYeh <yehyeh@users.noreply.github.com>
2024-08-01 00:08:26 +0300

    Initial Commit

---
+++ MoviesCatalog/MoviesCatalogApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesCatalogApp.swift
+//  MoviesCatalog
+//
+//  Created by Yehonatan Yehudai on 01/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct MoviesCatalogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MoviesCatalogUITests/MoviesCatalogUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MoviesCatalogUITestsLaunchTests.swift
+//  MoviesCatalogUITests
+//
+//  Created by Yehonatan Yehudai on 01/08/2024.
+//
+
+import XCTest
+
+final class MoviesCatalogUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
